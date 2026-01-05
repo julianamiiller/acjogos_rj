468a991fb046a25ff11424093525ecc576f334ef
Juliana Miiller <julianamiillergithub@gmail.com>
2026-01-05 19:39:04 -0300

    feat: configure password reset urls and email console backend

--- setup/urls.py
+++ setup/urls.py
@@ -7,12 +7,20 @@ from perfis.views import cadastro_view
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Rota personalizada de cadastro
     path('accounts/cadastro/', cadastro_view, name='cadastro'), 
+    
+    # Esta linha abaixo já inclui TODAS as rotas de login, logout e reset de senha
+    # As rotas de reset serão: accounts/password_reset/, accounts/password_reset/done/, etc.
     path('accounts/', include('django.contrib.auth.urls')),
+    
+    # Home protegida (só acessa se estiver logado)
     path('', login_required(TemplateView.as_view(template_name='home.html')), name='home'),
 ]
+
+# Configuração para arquivos estáticos (CSS, JS, Imagens) em modo de desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
