27f8ca714921a79d407863c5ca89810507588109
Juliana Miiller <julianamiillergithub@gmail.com>
2026-01-14 20:13:38 -0300

    feat(perfis): customize UserAdmin with PerfilInline and list filters

--- perfis/admin.py
+++ perfis/admin.py
@@ -3,12 +3,13 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import Perfil
 
+# 1. Configuração do Inline (para aparecer dentro da página de Usuário)
 class PerfilInline(admin.StackedInline):
     model = Perfil
     can_delete = False
     verbose_name_plural = 'Informações do Responsável (ACJOGOS-RJ)'
     
-    # Organiza os novos campos em grupos visuais no Admin
+    # Organiza os campos em grupos visuais
     fieldsets = (
         ('Vínculo e Contato', {
             'fields': ('tipo_usuario', 'telefone_contato', 'discord_nick')
@@ -21,9 +22,9 @@ class PerfilInline(admin.StackedInline):
         }),
     )
 
+# 2. Configuração do UserAdmin customizado
 class CustomUserAdmin(UserAdmin):
     inlines = (PerfilInline,)
-    # Mantive o list_display conforme sua base original
     list_display = ('username', 'email', 'first_name', 'last_name', 'get_tipo_vinculo', 'is_staff')
     
     def get_tipo_vinculo(self, obj):
@@ -34,6 +35,13 @@ class CustomUserAdmin(UserAdmin):
     
     get_tipo_vinculo.short_description = 'Tipo de Vínculo'
 
-# Re-registra o UserAdmin customizado
+# Re-registra o UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+
+# 3. Registro independente do Perfil (para aparecer na lista de apps do Admin)
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tipo_usuario', 'status')
+    list_filter = ('tipo_usuario', 'status')
+    search_fields = ('user__username', 'user__email', 'cpf') # Adicionei username e cpf para facilitar a busca
