7ca56d58ff7f68c2fa6b138c73c20ad56a2c2ee8
Juliana Miiller <julianamiillergithub@gmail.com>
2026-01-30 10:10:13 -0300

    feat: Initialize Django project settings with environment variable integration for database, static files, and email, and adjust gitignore.

--- setup/settings.py
+++ setup/settings.py
@@ -1,17 +1,22 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv 
+import dj_database_url
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-muda-isso-no-env')
+# Lê do ambiente. Se não achar, usa uma chave insegura (fallback)
+SECRET_KEY = os.getenv("SECRET_KEY", "chave-insegura-fallback")
+# Lê do ambiente. Retorna 'True' se o valor for "True", senão False.
+DEBUG = os.getenv("DEBUG", "False") == "True"
+# Hosts permitidos
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+# O Render define a variável RENDER_EXTERNAL_HOSTNAME automaticamente
+render_host = os.getenv("RENDER_EXTERNAL_HOSTNAME")
+if render_host:
+    ALLOWED_HOSTS.append(render_host)
 
-
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
-
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
-
+load_dotenv(BASE_DIR / ".env")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -29,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -59,12 +65,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 # Banco de dados
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+"default": dj_database_url.config(
+default=os.getenv("DATABASE_URL"),
+conn_max_age=600,
+ssl_require=True, # Importante para Render
+)
 }
-
 # Validação de Senhas
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -98,12 +104,12 @@ USE_TZ = True
 
 # Arquivos CSS, JavaScript, Imagens
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Configurações de Login/Logout
 LOGIN_REDIRECT_URL = '/pos-login/' #depois do login, nao vá direto para a home, vá primeiro para a minha logica
